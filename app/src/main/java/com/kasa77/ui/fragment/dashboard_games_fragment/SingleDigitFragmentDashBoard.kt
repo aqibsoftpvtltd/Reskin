26513ae4a9ec3ac6649916d7c2ef1f15de2ec602
Aqib SoftPvt <81578815+aqibsoftpvtltd@users.noreply.github.com>
2023-10-03 02:04:34 +0500

    ok

--- app/src/main/java/com/kasa77/ui/fragment/dashboard_games_fragment/SingleDigitFragmentDashBoard.kt
+++ app/src/main/java/com/kasa77/ui/fragment/dashboard_games_fragment/SingleDigitFragmentDashBoard.kt
@@ -72,7 +72,7 @@ class SingleDigitFragmentDashBoard : Fragment(), View.OnClickListener {
 
 
     private fun initViews() {
-        tabAddBid = rootView!!.findViewById<FrameLayout>(R.id.tabAddBid)
+        tabAddBid = rootView!!.findViewById(R.id.tabAddBid)
         tvFinalSubmit = rootView!!.findViewById(R.id.tvFinalSubmit)
         actDigits = rootView!!.findViewById(R.id.actDigits)
         etPoints = rootView!!.findViewById(R.id.etPoints)
@@ -273,7 +273,7 @@ class SingleDigitFragmentDashBoard : Fragment(), View.OnClickListener {
     private var gameTypeName = ""
     private var gameTypePrice = "0"
     private var tvFinalSubmit: TextView? = null
-    private var tabAddBid: FrameLayout? = null
+    private var tabAddBid: TextView? = null
     private var rootView: View? = null
     private var dbCnt = ""
     private var dbPnt = ""
